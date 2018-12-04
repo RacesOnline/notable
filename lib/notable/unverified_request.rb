c9e8a00bd2f3d519c38f9940240afaf51e4d16e6
Sakir Temel <sakirtemel@users.noreply.github.com>
2018-12-04 11:00:50 +0100

    Update unverified_request.rb

--- lib/notable/unverified_request.rb
+++ lib/notable/unverified_request.rb
@@ -1,18 +1,5 @@
 module Notable
   module UnverifiedRequest
-    extend ActiveSupport::Concern
-
-    included do
-      prepend_before_action :track_unverified_request
-    end
-
-    def track_unverified_request
-      if !verified_request?
-        expected = form_authenticity_token
-        actual = form_authenticity_param || request.headers["X-CSRF-Token"]
-        Notable.track "Unverified Request", "#{actual || "nil"} != #{expected}"
-      end
-    end
   end
 end
 
